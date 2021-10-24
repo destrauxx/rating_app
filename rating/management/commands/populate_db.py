b6687dd1629d0c62f643e03546ef68aa1e7b83de
paurbx <gleroapachii@gmail.com>
2021-10-24 15:44:29 +0300

    Adding tracking of adding a record to the database through the terminal, explaining comments in the code

--- rating/management/commands/populate_db.py
+++ rating/management/commands/populate_db.py
@@ -4,27 +4,41 @@ from rating.models import Rating
 
 class Command(BaseCommand):
     help = 'adding some entries to Rating models'
-    missing_args_message = 'kek'
+    missing_args_message = 'You do not entry the number of required to create rating nodes'
     def add_arguments(self, parser):
         parser.add_argument('count', type=int)
     
     def handle(self, *args, **options):
         from random import randint
         from bs4 import BeautifulSoup
-        import requests 
-        name_creator = 'https://www.name-generator.org.uk/quick/'
+        import requests
 
-        name_page = requests.get(URL).text
-        name_parser = BeautifulSoup(name_page, 'html.parser')
-        name_data = {}
+        from colorama import init, Fore
+        init()
 
-        for row in name_parser.find_all(class_ = 'name_heading'):
-            ids, name = row.find_all('td')[0].get_text(), row.find_all('td')[1].get_text()
-            name_data[name] = ids
-            print(ids, name, name_data)
+        name_generator = 'https://www.name-generator.org.uk/quick/'
         count = options.get('count', 1)
+
         for i in range(count):
-            r = Rating(name=i, text='created from command line', rate=randint(1, 5))
+            # Creation of a name by parsing from the site of the name generator,
+            # formatting it to match the format of the record name.
+            name_page = requests.get(name_generator).text
+            name_parser = BeautifulSoup(name_page, 'html.parser')
+            name = name_parser.find(class_ = 'name_heading').get_text().split()
+            name = name[0] + "_" + name[1] + str(randint(1, 10))
+
+            # Making a copy of the number of records in the database before creating the record
+            count_ratings = Rating.objects.count()
+
+            # Creating a record in the database and saving it
+            r = Rating(name=name, text='Created from command line', rate=randint(1, 5))
             r.save()
-            print('Element have created successfuly!')
-        print('Done!')
+
+            # Displaying the status of record creation in accordance with the database data
+            if count_ratings < Rating.objects.count():
+                print(Fore.LIGHTGREEN_EX + f'Element {name} have created successfully with rating {r.rate}!' + Fore.WHITE)   
+            else:
+                print(Fore.RED + f'Element {name} have not created!' + Fore.WHITE)
+
+        # Displaying information about the completion of work on the creation of a rating record
+        print(Fore.BLUE + "\nCompleted element creation!" + Fore.WHITE)
--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
 import requests 
+from colorama import init, Fore, Back
 name_creator = 'https://www.name-generator.org.uk/quick/'
 
 name_page = requests.get(name_creator).text
 name_parser = BeautifulSoup(name_page, 'html.parser')
 name_data = []
 
-count = 2
-name = [i.get_text() for i in name_parser.find_all(class_ = 'name_heading')[:count]]
-print(name)
+name = name_parser.find(class_ = 'name_heading').get_text().split()
+
+print(name[0] + '_' + name[1])
